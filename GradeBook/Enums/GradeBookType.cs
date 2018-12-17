471bb1ed26185c84c7ce76424a4bd854229d956f
Andrew Pear <pear.andrew@gmail.com>
2018-12-17 19:39:57 +0100

    Creating New GradeBookTypes
    Add enum GradeBooksType, Type property, Created class Ranked and Standart GradeBook

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standart = 1,
+        Ranked,
+        ESNU,
+        OneToFour
+    } 
+}
